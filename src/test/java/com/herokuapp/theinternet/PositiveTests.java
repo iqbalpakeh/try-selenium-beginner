8277e4fb6b322800a470f8f0273d6c84c5ca4b3d
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-01 06:44:26 +0700

    Adding positive test

---
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,21 @@
+package com.herokuapp.theinternet;
+
+public class PositiveTests {
+	
+	public void loginTest() {
+
+		// Create web driver
+		
+		// Open test page
+		
+		// Enter password 
+		
+		// Click login button
+		
+		// Verification
+		// - new url 
+		// - logout button is visible
+		// - succesful login message
+	}
+
+}
