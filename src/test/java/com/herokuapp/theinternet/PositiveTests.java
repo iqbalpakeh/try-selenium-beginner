49958d3f5c747cbd8c3be45002af050f1a5e71e5
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-01 07:04:37 +0700

    Open web page with chromedriver

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,21 +1,40 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class PositiveTests {
-	
+
+	@Test
 	public void loginTest() {
 
+		System.out.println("Starting login test");
+
 		// Create web driver
-		
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+		WebDriver driver = new ChromeDriver();
+
+		// Maximize browser window
+		driver.manage().window().maximize();
+
 		// Open test page
-		
-		// Enter password 
-		
+		String url = "https://the-internet.herokuapp.com/login";
+		driver.get(url);
+		System.out.println("Open test page");
+
+		// Enter password
+
 		// Click login button
-		
+
 		// Verification
-		// - new url 
+		// - new url
 		// - logout button is visible
 		// - succesful login message
+
+		// Close browser
+		driver.quit();
+
 	}
 
 }
