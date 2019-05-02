861747a34b469f46d103059583dcccb5a669715c
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-03 06:29:07 +0700

    Implement negative test

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -40,7 +40,7 @@ public class NegativeTests {
 
         // Enter username
         WebElement username = mDriver.findElement(By.id("username"));
-        username.sendKeys("tomsmith-false");
+        username.sendKeys("incorrect-username");
 
         // Enter password
         WebElement password = mDriver.findElement(By.name("password"));
@@ -63,6 +63,56 @@ public class NegativeTests {
         // Error login message
         WebElement successMessage = mDriver.findElement(By.xpath("//div[@id='flash']"));
         String expectedMsg = "Your username is invalid!";
+        String actualMsg = successMessage.getText();
+        Assert.assertTrue(actualMsg.contains(expectedMsg),
+                "Expected message is not the same as expected\nActual Message: " + actualMsg + "\nExpected Message: "
+                        + expectedMsg);
+
+    }
+
+    @Test
+    public void incorrectPasswordTest() {
+
+        // ---------------------------------------------------------------------------------------------
+        // Execution
+        // ---------------------------------------------------------------------------------------------
+
+        Util.log("Starting login test");
+
+        // Maximize browser window
+        mDriver.manage().window().maximize();
+
+        // Open test page
+        String url = "https://the-internet.herokuapp.com/login";
+        mDriver.get(url);
+        Util.log("Open test page");
+        Util.sleep(1000);
+
+        // Enter username
+        WebElement username = mDriver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+
+        // Enter password
+        WebElement password = mDriver.findElement(By.name("password"));
+        password.sendKeys("incorrect-password!");
+
+        // Click login button
+        WebElement loginButton = mDriver.findElement(By.tagName("button"));
+        loginButton.click();
+        Util.sleep(1000);
+
+        // ---------------------------------------------------------------------------------------------
+        // Verification
+        // ---------------------------------------------------------------------------------------------
+
+        // Check expected url
+        String expectedUrl = "https://the-internet.herokuapp.com/login";
+        String actualUrl = mDriver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same expected");
+
+        // Error login message
+        WebElement successMessage = mDriver.findElement(By.xpath("//div[@id='flash']"));
+        String expectedMsg = "Your password is invalid!";
         String actualMsg = successMessage.getText();
         Assert.assertTrue(actualMsg.contains(expectedMsg),
                 "Expected message is not the same as expected\nActual Message: " + actualMsg + "\nExpected Message: "
