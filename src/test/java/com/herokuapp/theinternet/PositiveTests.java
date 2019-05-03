c9016eb1ed331db8f88012a649c6702a587898f8
Progrema Studio <iqbalpakeh@gmail.com>
2019-05-03 23:23:45 +0700

    Refactor global variable to use m-prefix

--- src/test/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -4,7 +4,7 @@ import com.herokuapp.theinternet.util.Util;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,12 +12,12 @@ import org.testng.annotations.Test;
 
 public class PositiveTests {
 
-    WebDriver webDriver;
+    private WebDriver mDriver;
 
     @BeforeTest
     public void prepare() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        webDriver = new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        mDriver = new FirefoxDriver();
     }
 
     @Test
@@ -30,24 +30,24 @@ public class PositiveTests {
         Util.log("Starting login test");
 
         // Maximize browser window
-        webDriver.manage().window().maximize();
+        mDriver.manage().window().maximize();
 
         // Open test page
         String url = "https://the-internet.herokuapp.com/login";
-        webDriver.get(url);
+        mDriver.get(url);
         Util.log("Open test page");
         Util.sleep(1000);
 
         // Enter username
-        WebElement username = webDriver.findElement(By.id("username"));
+        WebElement username = mDriver.findElement(By.id("username"));
         username.sendKeys("tomsmith");
 
         // Enter password
-        WebElement password = webDriver.findElement(By.name("password"));
+        WebElement password = mDriver.findElement(By.name("password"));
         password.sendKeys("SuperSecretPassword!");
 
         // Click login button
-        WebElement loginButton = webDriver.findElement(By.tagName("button"));
+        WebElement loginButton = mDriver.findElement(By.tagName("button"));
         loginButton.click();
         Util.sleep(1000);
 
@@ -57,15 +57,15 @@ public class PositiveTests {
 
         // Check expected url
         String expectedUrl = "https://the-internet.herokuapp.com/secure";
-        String actualUrl = webDriver.getCurrentUrl();
+        String actualUrl = mDriver.getCurrentUrl();
         Assert.assertEquals(actualUrl, expectedUrl, "Actual page url is not the same expected");
 
         // logout button is displayed
-        WebElement logoutButton = webDriver.findElement(By.xpath("//a[@class='button secondary radius']"));
+        WebElement logoutButton = mDriver.findElement(By.xpath("//a[@class='button secondary radius']"));
         Assert.assertTrue(logoutButton.isDisplayed(), "Logout button is not displayed");
 
         // successful login message
-        WebElement successMessage = webDriver.findElement(By.xpath("//div[@id='flash']"));
+        WebElement successMessage = mDriver.findElement(By.xpath("//div[@id='flash']"));
         String expectedMsg = "You logged into a secure area!";
         String actualMsg = successMessage.getText();
         Assert.assertTrue(actualMsg.contains(expectedMsg),
@@ -76,7 +76,7 @@ public class PositiveTests {
 
     @AfterTest
     public void close() {
-        webDriver.quit();
+        mDriver.quit();
     }
 
 }
